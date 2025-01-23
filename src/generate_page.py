78c1f94d40e9c0964e8e9148ab6d322f99ec9155
Stefan Irwin <s2irwin@gmail.com>
2025-01-23 14:44:19 -0500

    working on function to generate HTML page

--- src/generate_page.py
+++ src/generate_page.py
@@ -13,3 +13,6 @@ def generate_page(from_path, template_path, dest_path):
         None
     """
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+
+    with open(from_path, "r") as md_file:
+        content = md_file.read()
